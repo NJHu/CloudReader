a15217ab56cd7219b426a538bfd399656cfe206a
胡鹏 <64hp@163.com>
2020-07-22 21:17:43 +0800

    处理网络工具类和Demo

---
+++ app/src/main/java/com/github/njhu/njcloudreader/Tool/AppService.kt
@@ -0,0 +1,11 @@
+package com.github.njhu.njcloudreader.Tool
+
+
+import retrofit2.http.GET
+import retrofit2.Call
+import com.github.njhu.njcloudreader.Bean.Article as Article
+
+interface AppService {
+    @GET("files/articleList.json")
+    fun getArticleList(): Call<List<Article>>
+}
